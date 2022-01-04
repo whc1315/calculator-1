1de0332c614ba69cffa836f4870b48443dc73b63
William Cuthbertson <whc1315@gmail.com>
2022-01-04 14:26:03 -0700

    Subtraction implementation working

--- calc.js
+++ calc.js
@@ -18,8 +18,10 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 + num2)
 	}
 
+	if (mathSymbol === "-"){
+		console.log(num1 - num2)
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
-console.log('mathSymbol', mathSymbol)
 });
